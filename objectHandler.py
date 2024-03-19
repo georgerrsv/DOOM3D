03ccf1aa4feff48e2f2a8357baa652117023cec8
George <georgerrsilva@alu.ufc.br>
2024-03-19 09:46:37 -0300

    adding sprites

--- objectHandler.py
+++ objectHandler.py
@@ -10,6 +10,15 @@ class objectHandler:
 
         addSprite(spriteObject(game))
         addSprite(animatedSprites(game))
+        addSprite(animatedSprites(game, path=self.animatedSpritePath + 'redLight/0.png', pos=(14.5, 7.5)))
+        addSprite(animatedSprites(game, path=self.animatedSpritePath + 'redLight/0.png', pos=(4.5, 7.5)))
+        addSprite(animatedSprites(game, path=self.animatedSpritePath + 'greenLight/0.png', pos=(11.5, 7.5)))
+        addSprite(animatedSprites(game, path=self.animatedSpritePath + 'redLight/0.png', pos=(3.5, 7.5)))
+        addSprite(animatedSprites(game, path=self.animatedSpritePath + 'redLight/0.png', pos=(2.5, 7.5)))
+        addSprite(animatedSprites(game, path=self.animatedSpritePath + 'redLight/0.png', pos=(1.5, 7.5)))
+        addSprite(animatedSprites(game, path=self.animatedSpritePath + 'redLight/0.png', pos=(8.5, 7.5)))
+        addSprite(animatedSprites(game, path=self.animatedSpritePath + 'greenLight/0.png', pos=(6.5, 7.5)))
+        addSprite(animatedSprites(game, path=self.animatedSpritePath + 'greenLight/0.png', pos=(1.5, 1.5)))
 
     def update(self):
         [sprite.update() for sprite in self.spriteList]
