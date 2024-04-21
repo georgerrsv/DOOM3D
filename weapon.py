41cb7e71fb10311a367050d8afe2a4718a1fb7ef
George <georgerrsilva@alu.ufc.br>
2024-04-21 10:38:23 -0300

    adding weapon sprite and sound

--- weapon.py
+++ weapon.py
@@ -1,18 +1,19 @@
 from spriteObject import *
 
+
 class Weapon(animatedSprites):
     def __init__(self, game, path='resources/sprites/weapon/shotgun/0.png', scale=0.4, animationTime=90):
         super().__init__(game=game, path=path, scale=scale, animationTime=animationTime)
         self.images = deque(
             [pg.transform.smoothscale(img, (self.image.get_width() * scale, self.image.get_height() * scale))
-                for img in self.images])
+             for img in self.images])
         self.weaponPosition = (HALF_WIDTH - self.images[0].get_width() // 2, HEIGHT - self.images[0].get_height())
         self.reloading = False
         self.numImages = len(self.images)
         self.frameCounter = 0
         self.damage = 50
 
-    def animateShot(self):
+    def animatedShot(self):
         if self.reloading:
             self.game.player.shot = False
             if self.animationTrigger:
@@ -23,9 +24,9 @@ class Weapon(animatedSprites):
                     self.reloading = False
                     self.frameCounter = 0
 
-    def update(self):
-        self.checkAnimationTime()
-        self.animateShot()
-
     def draw(self):
         self.game.screen.blit(self.images[0], self.weaponPosition)
+
+    def update(self):
+        self.checkAnimationTime()
+        self.animatedShot()
